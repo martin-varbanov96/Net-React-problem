ecacdf3d3b93f3fc0d81cee9a05252f1107f0119
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-09-20 10:29:04 +0300

    Comments
    adding comments

--- NetReactProblem/problem.php
+++ NetReactProblem/problem.php
@@ -1,42 +1,61 @@
 <?php
 ini_set('max_execution_time', 300);
-$n=rand();
-$m=rand();
+$n = rand();
+$m = rand();
 
-$simpleArr=simple($n,$m);
-$duplicatedArr=array_count_values($simpleArr);
-$simpleArr=array_unique($simpleArr);
+$simpleArr = simple($n, $m);
+$duplicatedArr = array_count_values($simpleArr);
+$simpleArr = array_unique($simpleArr);
 arsort($duplicatedArr);
-
 printKey($duplicatedArr);
 
-function printKey($arr){
-    $tempMax=0;
-	echo "the most common duplicates are: ";
-    foreach($arr as $key=>$var){
-        if($var>=$tempMax){
-            $tempMax=$var;
-            echo $key."\t";
-        }else{
+/**
+ * Prints the most common duplicates
+ * @param $arr - an array of duplicates
+ */
+function printKey($arr)
+{
+    $tempMax = 0;
+    echo "the most common duplicates are: ";
+    foreach ($arr as $key => $var) {
+        if ($var >= $tempMax) {
+            $tempMax = $var;
+            echo $key . "\t";
+        } else {
             break;
         }
     }
 }
 
-function simple($number,$max){
-    $outputArr=[];
-    while(count($outputArr)!=$number){
-        $tempRand=rand(2,$max);
-        if(isSimple($tempRand)){
-            array_push($outputArr,$tempRand);
+/**
+ * Creates an array of simple numbers
+ * @param $number - the length of the array
+ * @param $max - the max value of the elements
+ * @return array - the simple number array
+ */
+ 
+function simple($number, $max)
+{
+    $outputArr = [];
+    while (count($outputArr) != $number) {
+        $tempRand = rand(2, $max);
+        if (isSimple($tempRand)) {
+            array_push($outputArr, $tempRand);
         }
     }
     return $outputArr;
 }
 
-function isSimple($var){
-    for($i=2;$i<$var;$i++){
-        if($var%$i==0){
+/**
+ * Checks if the variable is simple
+ * @param $var - variable
+ * @return bool - the result
+ */
+ 
+function isSimple($var)
+{
+    for ($i = 2; $i < $var; $i++) {
+        if ($var % $i == 0) {
             return false;
         }
     }
